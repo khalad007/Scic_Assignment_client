74d385464e98a75d6070cc2ade8d3cf4173ef7e1
khalad007 <www.arifhossain49919@gmail.com>
2023-12-21 15:09:47 +0600

    Basic Setup & Router Setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+// import Banner from "../Banner/Banner";
+
+import Banner from "../Banner/Banner";
+
+
+
+const Home = () => {
+    return (
+        
+          <div></div>
+
+        
+    );
+};
+
+export default Home;
